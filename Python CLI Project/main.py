510a3ea6b73ad8fd47337abd7c0caf6c9bc5bff3
vignesh103003 <vk7937605@gmail.com>
2026-06-24 14:54:25 +0530

    db

--- Python CLI Project/main.py
+++ Python CLI Project/main.py
@@ -1,1 +1,9 @@
-print("Hello, World!")
+import mysql.connector
+connection = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="password",
+    database="hostel_db"
+)
+if connection.is_connected():
+    print("Connected Successfully")
